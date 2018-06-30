747be449ae44c78850288239fed1749a61b616e5
DM Coulter <dcoult01@seattlecentral.edu>
2018-06-30 10:16:13 -0700

    Optional "Satisfied Clients" icons info

---
+++ multi-site/wp-content/themes/blankslate/template-part/clients.php
@@ -0,0 +1,12 @@
+<div class="container">
+<h2>Here are just some of my satisfied clients:</h2>
+            <ul class="clients">
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/lynch.jpeg" alt="myrle lynch"></li>
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/mcdonalds.jpg" alt="mcdonalds"></li>
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/under-armor.jpg" alt="under armor"></li>
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/yankee.jpg" alt="yankee candle"></li>
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/walmart.jpg" alt="walmart"></li>
+                <li><img src="http://www.velvetriot.com/portfolio/wp-content/uploads/fifth-third.jpg" alt="fift-third"></li>
+                <!--<li><img src="images/finish-line.jpeg" alt="finish line"></li>-->
+            </ul>
+        </div><!--end Clients container div—>
